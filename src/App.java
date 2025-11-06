a699c604a84d060a43ca37f51c22971cfc032417
Curro Portillo Guerrero <cporgue2307@g.educaand.es>
2025-11-06 11:01:44 +0100

    ejercicio 35

--- src/App.java
+++ src/App.java
@@ -4,14 +4,14 @@ public class App {
 
         do{
             altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura de la X (<=3): "));
-            if(altura<3 || altura % 2 == 0)
+            if(altura < 3 || altura % 2 == 0)
                 System.out.println("Datos incorrectos. Debe introducir una altura impar mayor o igual a 3");
         }while(altura < 3 || altura % 2 == 0);
         int contador = 0;
         int fila = altura;
         int espacios = 0;
 
-        while(contador<altura){
+        while(contador<altura/2){
             for(int i = 1; i<= espacios; i++){
                 System.out.print(" ");
             }
@@ -26,5 +26,23 @@ public class App {
             fila -= 2;
             contador++;
         }
+        fila = 1;
+        espacios = altura / 2 + 1;
+        while(contador<altura){
+            for(int i = 1; i < espacios; i++){
+                System.out.print(" ");
+            }
+            for(int i = 1; i <= fila; i++){
+                if(i == 1 || i == fila)
+                    System.out.print("*");
+                    else
+                        System.out.print(" ");
+            }
+            System.out.println();
+            contador++;
+            fila+=2;
+            espacios--;
+        }
+        
     }
 }
